334404691e983ab24dadee212eda895308fdff64
frauke227 <Frauke.Frahm@gmail.com>
2021-06-06 23:27:08 +0200

    first commit, ConnectionRefusedError when trying to request pw reset mail

---
+++ flaskblog/__init__.py
@@ -0,0 +1,23 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+from flask_mail import Mail
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get('DB_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+app.config['MAIL_Server'] = 'smtp.gmail.com'
+app.config['MAIL_Port'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+mail = Mail(app)
+from flaskblog import routes
+
